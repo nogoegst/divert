d17e9800df5805ce0176f129a24c8daf06c94e1f
Ivan Markin <twim@riseup.net>
2017-04-27 22:25:40 +0000

    Remove bloat

--- divert.go
+++ divert.go
@@ -15,35 +15,34 @@ import (
 	"os"
 	"strconv"
 	"syscall"
-	"time"
 )
 
 const IPPROTO_DIVERT = 258
 
-type DivertAddr struct {
+type Addr struct {
 	Port uint16
 }
 
-func (a DivertAddr) Network() string {
+func (a *Addr) Network() string {
 	return "divert"
 }
 
-func (a DivertAddr) String() string {
+func (a *Addr) String() string {
 	return strconv.FormatUint(uint64(a.Port), 16)
 }
 
-type DivertConn struct {
-	addr DivertAddr
+type Conn struct {
+	addr *Addr
 	fd   int
 	sa   syscall.Sockaddr
 }
 
-func (c *DivertConn) Read(b []byte) (int, error) {
+func (c *Conn) Read(b []byte) (int, error) {
 	n, _, err := syscall.Recvfrom(c.fd, b, 0)
 	return n, err
 }
 
-func (c *DivertConn) Write(b []byte) (int, error) {
+func (c *Conn) Write(b []byte) (int, error) {
 	err := syscall.Sendto(c.fd, b, 0, c.sa)
 	if err != nil {
 		return 0, os.NewSyscallError("socket", err)
@@ -51,37 +50,14 @@ func (c *DivertConn) Write(b []byte) (int, error) {
 	return len(b), nil
 }
 
-func (c *DivertConn) Close() error {
+func (c *Conn) Close() error {
 	return syscall.Close(c.fd)
 }
 
-func (c *DivertConn) LocalAddr() net.Addr {
-	return c.addr
-}
-
-func (c *DivertConn) RemoteAddr() net.Addr {
-	return c.addr
-}
-
-func (c *DivertConn) SetDeadline(t time.Time) error {
-	return errors.New("pointless")
-}
-
-func (c *DivertConn) SetReadDeadline(t time.Time) error {
-	return errors.New("pointless")
-}
-
-func (c *DivertConn) SetWriteDeadline(t time.Time) error {
-	return errors.New("pointless")
-}
-func Listen(network, address string) (net.Conn, error) {
+func Listen(network, address string) (*Conn, error) {
 	if network != "divert" {
 		return nil, errors.New("network is not divert")
 	}
-	return ListenDivert(address)
-}
-
-func ListenDivert(address string) (*DivertConn, error) {
 	s, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_RAW, IPPROTO_DIVERT)
 	if err != nil {
 		return nil, os.NewSyscallError("socket", err)
@@ -95,5 +71,5 @@ func ListenDivert(address string) (*DivertConn, error) {
 		syscall.Close(s)
 		return nil, os.NewSyscallError("bind", err)
 	}
-	return &DivertConn{fd: s, addr: DivertAddr{Port: uint16(port)}, sa: sa}, nil
+	return &Conn{fd: s, addr: &Addr{Port: uint16(port)}, sa: sa}, nil
 }
